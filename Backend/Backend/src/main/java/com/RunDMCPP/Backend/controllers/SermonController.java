014220977d448b0d4ef502ed332fc87535aa8119
KevCendana <kevincendana@csus.edu>
2023-11-11 14:39:19 -0800

    DT-104
    Fulfilled JIRA Acceptance Criteria:
    - DB must have table to store Sermon objects
    - DB must store titles, descriptions, times, dates, YT links
    - Backend must be able to CRUD sermon objects

--- Backend/Backend/src/main/java/com/RunDMCPP/Backend/controllers/SermonController.java
+++ Backend/Backend/src/main/java/com/RunDMCPP/Backend/controllers/SermonController.java
@@ -1,12 +1,64 @@
+// SermonController.java
 package com.RunDMCPP.Backend.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import com.RunDMCPP.Backend.models.Sermon;
+import com.RunDMCPP.Backend.services.SermonService;
+import java.util.List;
+import java.util.Optional;
 
-@RestController
-@RequestMapping("/api/Sermons")
+@RestController                                   
+@RequestMapping("/api/DynamoDbPrototype/sermons") 
 public class SermonController {
+    @Autowired
+    private SermonService sermonService;
+
+    @GetMapping
+    @RequestMapping("/all")
+    public ResponseEntity<List<Sermon>> getAllSermons(){
+        List<Sermon> sermons = sermonService.findAll();
+        return new ResponseEntity<>(sermons, HttpStatus.OK);
+    }
+
+    @GetMapping
+    @RequestMapping("/{id}")
+    public Optional<Sermon> getSermonByID(@PathVariable String id){
+        return sermonService.findById(id);
+    }
+
+     @PostMapping 
+    public ResponseEntity<Sermon> addSermon(@RequestBody final Sermon s){
+        Sermon result = sermonService.createSermon(s);
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}") 
+    public ResponseEntity<Sermon> updateSermon(@PathVariable String id, @RequestBody Sermon s) {
+        return new ResponseEntity<>(sermonService.editSermon(s), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Sermon> deleteSermon(@RequestBody Sermon s){
+        sermonService.deleteSermon(s);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    // todo: api endpoint to search sermons by title and by date range; i tried but uhhhhh yeah i'm not sure how to do it
+    @GetMapping("/search/title/{title}")
+    public ResponseEntity<List<Sermon>> searchByTitle(@PathVariable String title) {
+        List<Sermon> sermons = sermonService.searchSermonsByTitle(title);
+        return new ResponseEntity<>(sermons, HttpStatus.OK);
+    }
+    @GetMapping("/search/date")
+    public ResponseEntity<List<Sermon>> searchByDateRange(
+            @RequestParam String startDate,
+            @RequestParam String endDate) {
+        List<Sermon> sermons = sermonService.searchSermonsByDateRange(startDate, endDate);
+        return new ResponseEntity<>(sermons, HttpStatus.OK);
+    }
+    
 
 }
