a3300fd80fd8d5613152a605cfb2fb59bdc2aca3
KevCendana <kevincendana@csus.edu>
2023-11-25 23:10:39 -0800

    DT 120 - Added backend comments
    Description:
     'Admins need to be able to see what BE and FE code does in the event that the website needs to be changed or updated.'
    
    Acceptance Criteria:
    'BE code has relevant and descriptive comments in each file'
    
    All .java files in src (except repositories cause they're blank) now have comments.

--- Backend/Backend/src/main/java/com/RunDMCPP/Backend/validation/SermonValidator.java
+++ Backend/Backend/src/main/java/com/RunDMCPP/Backend/validation/SermonValidator.java
@@ -3,17 +3,21 @@ package com.RunDMCPP.Backend.validation;
 import com.RunDMCPP.Backend.models.Sermon;
 import org.springframework.stereotype.Component;
 
+// SermonValidator class used to validate sermons.
 @Component
 public class SermonValidator {
-
+    // Method to validate reading (fetching) a sermon
     public boolean readValidator(Sermon input) {
+        // If the sermon has an id, it is valid
         if (input.getId() != null) {
             return true;
         }
-        return false;
+        return false; // Otherwise, it is invalid
     }
 
+    // Method to validate creating a sermon
     public boolean createValidator(Sermon input) {
+        // If event has desc, yt link, date/time, id, it is valid
         if (input.getName() != null
                 && input.getDescription() != null
                 && input.getYoutubeLink() != null
@@ -22,21 +26,23 @@ public class SermonValidator {
         ) {
             return true;
         }
-        return false;
+        return false; // Otherwise, it is invalid
     }
 
+    // Method to validate updating a sermon
     public boolean updateValidator(Sermon input, Sermon dbEntity) {
-
+        // If the sermon has an id & matches the one in the DB, it is valid
         if (input.getId() != null) {
             if (input.getId().equals(dbEntity.getId())) {
                 return true;
             }
         }
-        return false;
-    }
+        return false; // Otherwise, it is invalid
+    } 
 
+    // Method to validate deleting a sermon
     public boolean deleteValidator(Sermon input, Sermon dbEntity) {
-
+        // If sermon has name, desc, yt link, date/time & matches the one in the DB, it is valid
         if (input.getId() != null
                 && input.getName() != null
                 && input.getDescription() != null
@@ -47,6 +53,6 @@ public class SermonValidator {
                 return true;
             }
         }
-        return false;
+        return false; // Otherwise, it is invalid
     }
 }
