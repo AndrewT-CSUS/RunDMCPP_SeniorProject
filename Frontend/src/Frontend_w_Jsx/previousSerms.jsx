f535164996b774893c6b7c5496ff54f5695db2e0
Andrew <99375177+AndrewT-CSUS@users.noreply.github.com>
2024-02-11 03:06:56 -0800

    DT-125
    As part of unifying the date/time format, I assisted in moving the previous sermons page to the new react format. Had to do some rewriting anyways, why not help out.

--- Frontend/src/Frontend_w_Jsx/previousSerms.jsx
+++ Frontend/src/Frontend_w_Jsx/previousSerms.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {searchByName, searchByDate} from './Javascript/previousSerms.js'
 
 // Define your functional component
 function PreviousSerms() {
@@ -62,7 +63,37 @@ function PreviousSerms() {
             </header>
             <main>
                 {/* Main content goes here */}
-                <p>This is the Previous Sermons page</p>
+                <fieldset id="searchField" className="searchBox" >
+                    <legend>Sermon Search</legend>
+                    <h3>Name Search</h3>
+                    <div style={{display: "flex", alignItems: "center"}}>
+                        <textarea rows="1" id="searchBox" name="searchBox" placeholder="Sermon Name"></textarea><br></br>
+                    </div>
+                    <div><br /> </div>
+                    <div style={{display: "flex", flexWrap: "wrap",  alignItems: "left", justifyContent: "left"}}>
+                        <button type="button" id="searchByNameButton" onClick={searchByName}>Search By Name</button>
+                    </div>
+                    <div><br /> </div>
+                    <h3>Date Search</h3>
+                    <div>
+                        <p style={{margin: "0"}}>Start Date</p>
+                        <input type="date" id="startDateBox" name="startDateBox"/>
+                    </div>
+                    <div>
+                        <br/>
+                        <p style={{margin: "0"}}>End Date</p>
+                        <input type="date" id="endDateBox" name="endDateBox"/>
+                    </div>
+                    <div><br /> </div>
+                    <div style={{display: "flex", flexWrap: "wrap",  alignItems: "left", justifyContent: "left"}}>
+                        <button type="button" id="searchByDateButton" onClick={searchByDate} style={{ align: "left"}}>Search By Date</button>
+                    </div>
+                </fieldset>
+                <br></br>
+                <fieldset id="resultsField" className="resultBox" hidden>
+                    <legend id="resultsLegend">Search Result</legend>
+                    <h4 id="resultTitle">Title</h4>
+                </fieldset>
             </main>
             <footer>
                 {/* Footer content goes here */}
