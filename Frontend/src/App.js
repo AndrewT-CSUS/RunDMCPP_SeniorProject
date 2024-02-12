e96ebc9ea76a21ab36ea477d77c2bdc6e4a1943e
spjcyTofu <you@example.com>
2024-02-11 20:42:30 -0800

    Deleting Deprecated Pages cont.

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,34 +1,16 @@
 // Libraries: Allows linking to other pages, OAuth login
 import React from 'react';
-<<<<<<< Updated upstream
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Linking to other pages
 import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';  // OAuth Admin Login
 import { AdminProvider } from './components/Javascript/adminContext'; // Allows for admin privleges
-=======
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Schedule from './Frontend_w_Jsx/schedule';
-import AdminSermonAdd from './Frontend_w_Jsx/adminSermonAdd';
-import AdminSermonEdit from './Frontend_w_Jsx/adminSermonEdit';
-import AdminAnnouncementsAdd from './Frontend_w_Jsx/adminAnnouncementsAdd';
-import Announcements from './Frontend_w_Jsx/announcements';
-import Events from './Frontend_w_Jsx/events';
-import Languages from './Frontend_w_Jsx/languages';
-import OurBeliefs from './Frontend_w_Jsx/ourBeliefs';
-import OurGoals from './Frontend_w_Jsx/ourGoals';
-import PastEvents from './Frontend_w_Jsx/pastEvents';
-import PhotoGallery from './Frontend_w_Jsx/photoGallery';
-import PreviousSerms from './Frontend_w_Jsx/previousSerms';
-import Services from './Frontend_w_Jsx/services';
-import UpcomingSerms from './Frontend_w_Jsx/upcomingSerms';
->>>>>>> Stashed changes
 
 // Pages: Importing all the different pages
 import Schedule from './components/schedule';
+import AdminAnnouncementsAdd from './components/adminAnnouncementsAdd'
 import AdminSermonAdd from './components/adminSermonAdd';
 import AdminSermonEdit from './components/adminSermonEdit';
 import Announcements from './components/announcements';
 import Events from './components/events';
-import Languages from './components/languages';
 import OurBeliefs from './components/ourBeliefs';
 import OurGoals from './components/ourGoals';
 import PastEvents from './components/pastEvents';
@@ -61,7 +43,6 @@ function App() {
         <Link to="/adminAnnouncementsAdd">Admin Announcements Add</Link>
         <Link to="/announcements">Announcements</Link>
         <Link to="/events">Events</Link>
-        <Link to="/languages">Languages</Link>
         <Link to="/ourBeliefs">Our Beliefs</Link>
         <Link to="/ourGoals">Our Goals</Link>
         <Link to="/pastEvents">Past Events</Link>
@@ -81,7 +62,6 @@ function App() {
         <Route path="/adminAnnouncementsAdd" element={<AdminAnnouncementsAdd />} />
         <Route path="/announcements" element={<Announcements />} />
         <Route path="/events" element={<Events />} />
-        <Route path="/languages" element={<Languages />} />
         <Route path="/ourBeliefs" element={<OurBeliefs />} />
         <Route path="/ourGoals" element={<OurGoals />} />
         <Route path="/pastEvents" element={<PastEvents />} />
