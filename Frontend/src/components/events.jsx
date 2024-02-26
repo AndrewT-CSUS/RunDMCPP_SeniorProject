7b6d99a05d14f0b81e072228af347c1bea5ad263
IB OUATTARA <OUATT.TIEFFI@GMAIL.COM>
2024-02-25 23:16:21 -0800

    DT-117

--- Frontend/src/components/events.jsx
+++ Frontend/src/components/events.jsx
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getEvents } from './Javascript/fetchEvent';
 
-// Events Component
 function Events() {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetchEvents();
+  }, []);
+
+  const fetchEvents = async () => {
+    try {
+      const eventData = await getEvents(); // Use the getEvents function to fetch events
+      setEvents(eventData);
+    } catch (error) {
+      console.error('Error fetching events:', error);
+      setError('Error: Failed to fetch events -_-');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <>
-      <main>
-        {/* Main content goes here */}
-        <p>This is the events page</p>
-      </main>
-    </>
+
+    <main>
+      <h1>Events</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>{error}</p>
+      ) : events.length === 0 ? (
+        <p>No upcoming events</p>
+      ) : (
+        <div className="eventsList">
+          {events.map((event, index) => (
+            <div key={index} className="event">
+              <h2>{event.name}</h2>
+              <p>Date & Time: {new Date(event.dateTime).toLocaleString()}</p>
+              <p>Location: {event.eventLocation}</p>
+              <p>Description: {event.eventDescription}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </main>
+
   );
 }
 
