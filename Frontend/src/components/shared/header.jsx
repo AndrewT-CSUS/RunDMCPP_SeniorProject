ef27768c4c2312c39fd07753bf8ed05508cff924
spjcyTofu <you@example.com>
2024-04-27 21:31:43 -0700

    adminPage bug

--- Frontend/src/components/shared/header.jsx
+++ Frontend/src/components/shared/header.jsx
@@ -29,7 +29,7 @@ function Header() {
             {/* Conditional Div: Only display when the user is an admin. */}
             {isAdmin && (
                 <div className="admin-section">
-                    <a href={`${window.location.origin}/adminpage`}>You are logged in as Admin</a>
+                    <a href={`${window.location.origin}/adminPage`}>You are logged in as Admin</a>
                     <img src={starLogo} className="admin-star" alt="Admin Star"/>
                 </div>
             )}                            
